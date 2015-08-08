4de9236dcd86375c9620d11d5fd3a8274c3c3582
roycrippen <roycrippen@gmail.com>
2015-08-08 15:00:36 -0500

    initial source

--- hw6/src/Main.hs
+++ hw6/src/Main.hs
@@ -1,21 +1,10 @@
 module Main where
 
 import Hw6
-import Testing
-import Hw6Tests
-import Test.QuickCheck
-
 
 main :: IO()
 main = do
-    putStrLn "running unit tests..."
-    let a = runTests allTests
-    if null a
-        then putStrLn "all unit tests ok"
-        else print a
-    putStrLn "running property tests..."
-    quickCheck prop_Fibonacci1
-    quickCheck prop_Fibonacci2
+    putStrLn "Homework 6 main..."
     print ("fib 24 = 75025", fib 24)
     print ("first 25 fibs1 = ", take 25 fibs1)
     print ("first 25 fibs2 = ", take 25 fibs2)
