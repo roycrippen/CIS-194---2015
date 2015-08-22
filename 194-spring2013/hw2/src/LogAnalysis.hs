4d9c3717a97a43d670c4a704995eab1ef42f2b67
roycrippen <roycrippen@gmail.com>
2015-08-22 15:28:19 -0500

    initial sourcefinal

--- 194-spring2013/hw2/src/LogAnalysis.hs
+++ 194-spring2013/hw2/src/LogAnalysis.hs
@@ -8,20 +8,25 @@ import           Log
 -- exercise 1 ---------------
 parseMessage :: String -> LogMessage
 parseMessage "" = Unknown "empty log message"
-parseMessage msg =
-    case msgType of
-        "I" ->  LogMessage Info
-                           (read $ msgParts !! 1)
-                           (unwords $ drop 2 msgParts)
-        "W" ->  LogMessage Warning
-                           (read $ msgParts !! 1)
-                           (unwords $ drop 2 msgParts)
-        "E" ->  LogMessage (Error (read $ msgParts !! 1))
-                           (read $ msgParts !! 2)
-                           (unwords $ drop 3 msgParts)
-        _   -> Unknown msg
-    where msgParts = words msg
-          [msgType] = take 1 msgParts
+parseMessage msg = case msgType of
+                        "I" ->  parseInfo msgParts
+                        "W" ->  parseWarning msgParts
+                        "E" ->  parseError msgParts
+                        _   -> Unknown msg
+                   where msgParts = words msg
+                         [msgType] = take 1 msgParts
+
+parseInfo :: [String] -> LogMessage
+parseInfo (_:ts:msg) = LogMessage Info (read ts) (unwords msg)
+parseInfo _          = LogMessage Info 0 "malformed info message"
+
+parseWarning :: [String] -> LogMessage
+parseWarning (_:ts:msg) = LogMessage Warning (read ts) (unwords msg)
+parseWarning _          = LogMessage Warning 0 "malformed warning message"
+
+parseError :: [String] -> LogMessage
+parseError (_:pr:ts:msg) = LogMessage (Error (read pr)) (read ts) (unwords msg)
+parseError _             = LogMessage (Error 0) 0 "malformed error message"
 
 parse :: String -> [LogMessage]
 parse logStr = map parseMessage $ lines logStr
@@ -55,16 +60,16 @@ whatWentWrong msgs = map (\(LogMessage _ _ msg) -> msg) eGT49Sorted
     where
       eGT49 =
           filter (\(LogMessage (Error n) _ _) -> n > 49) $
-          filter(\(LogMessage mType _ _ ) -> mType /= Info && mType /= Warning) msgs
+          filter (\(LogMessage mType _ _ ) -> mType /= Info && mType /= Warning) msgs
       eGT49Sorted = inOrder $ build eGT49
+
+default (Int)
 
 testSolution :: FilePath -> IO [String]
 testSolution fp = do
     logFile <- readFile fp
-    let e = filter (\x -> head (words x) == "E") $ lines logFile
-        toInt :: String -> Int
-        toInt = read
-        eGT49 = filter (\x -> toInt(words x !! 1) > 49) e
-        s = map (\x -> (read (words x !! 2)::Int, unwords(drop 3 (words x)))) eGT49
+    let e = filter (\x -> take 1 (words x) == ["E"]) $ lines logFile
+        eGT49 = filter (\x -> (read $ words x !! 1 ::Int) > 49) e
+        s = map (\x -> (read $ words x !! 2 ::Int, unwords $ drop 3 $ words x)) eGT49
         s' = sortBy (compare `on` fst) s
     return (map snd s')
--- 194-spring2013/hw2/src/Main.hs
+++ 194-spring2013/hw2/src/Main.hs
@@ -1,14 +1,16 @@
 module Main where
 
-import           Log         (testWhatWentWrong)
+import           Log         (testWhatWentWrong, testParse)
 import           LogAnalysis (parse, testSolution, whatWentWrong)
 
 main :: IO ()
 main = do
     putStrLn "homework 2..."
+    testParsing <- testParse parse 100 "error.log"
+    mapM_ print testParsing
     error1  <- testWhatWentWrong parse whatWentWrong "error.log"
     error2 <- testSolution "error.log"
-    putStrLn "take 3 from testWhatWentWrong on error.log:"
+    putStrLn "\ntake 3 from testWhatWentWrong on error.log:"
     putStrLn "["
     mapM_ print (take 3 error1)
     putStrLn "...]\n\ntake 3 from testSolution on error.log:"
