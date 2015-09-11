d3716cada38af2029a5c31d940035eb70013d821
roycrippen <roycrippen@gmail.com>
2015-09-11 04:10:14 -0500

    final

---
+++ 194-spring2013/hw7/src/Scrabble.hs
@@ -0,0 +1,31 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+module Scrabble where
+
+import Data.Monoid
+import Data.Char (toLower)
+
+-- exercise 3 ----------------------------------
+newtype Score = Score Int
+    deriving (Eq, Ord, Show, Num)
+
+getScore :: Score -> Int
+getScore (Score i) = i
+
+instance Monoid Score where
+  mempty  = Score 0
+  mappend = (+)
+
+score :: Char -> Score
+score c
+    | c' `elem` "aeilnorstu" = Score 1
+    | c' `elem` "dg"         = Score 2
+    | c' `elem` "bcmp"       = Score 3
+    | c' `elem` "fhvwy"      = Score 4
+    | c' `elem` "k"          = Score 5
+    | c' `elem` "jx"         = Score 8
+    | c' `elem` "qz"         = Score 10
+    | otherwise              = Score 0
+        where c' = toLower c
+
+scoreString :: String -> Score
+scoreString s = mconcat $ map score s
