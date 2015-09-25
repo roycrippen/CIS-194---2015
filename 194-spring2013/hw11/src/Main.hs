ebf5df5486258b5ea89314d1eebf985d7345df63
roycrippen <roycrippen@gmail.com>
2015-09-25 03:41:38 -0500

    final

--- 194-spring2013/hw11/src/Main.hs
+++ 194-spring2013/hw11/src/Main.hs
@@ -1,9 +1,24 @@
 module Main where
 
 import           AParser
+import           SExpr
 
 main :: IO ()
 main = do
     putStrLn "homework 11...\n"
---    putStrLn $ "runParser intOrUppercase \"abc\" -> " ++ show (runParser intOrUppercase "abc")
+    putStrLn "\nparsing long string [1..10000] -> \"n abcn\""
+    putStrLn $ "..." ++ drop (length str' - 100) str'
+    putStrLn getExpr
     putStrLn "\ndone..."
+
+str :: String
+str = foldr (\x s -> ("(" ++ show x ++ " abc" ++ show x ++ ") " ++ s)) "" ([1..10000] :: [Int])
+
+str' :: String
+str' = "(" ++ str ++ ")"
+
+getExpr :: String
+getExpr = case runParser parseSExpr str' of
+    Nothing -> "Error parsing"
+    Just (v,_) -> "\n\n..." ++ drop (length s - 99) s
+        where s = show v
