d30a25665c29f8d30760dcc19b4cbc2b5b615e39
roycrippen <roycrippen@gmail.com>
2015-09-24 20:30:46 -0500

    initial

---
+++ 194-spring2013/hw11/src/Main.hs
@@ -0,0 +1,9 @@
+module Main where
+
+import           AParser
+
+main :: IO ()
+main = do
+    putStrLn "homework 11...\n"
+--    putStrLn $ "runParser intOrUppercase \"abc\" -> " ++ show (runParser intOrUppercase "abc")
+    putStrLn "\ndone..."
