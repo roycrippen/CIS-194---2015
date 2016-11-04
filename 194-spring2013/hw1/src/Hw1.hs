b0a7ebd5c43e98f33b5211cb3b470d6974347960
roy crippen <roycrippen@gmail.com>
2016-11-04 15:54:41 -0500

    adapt to stack

--- 194-spring2013/hw1/src/Hw1.hs
+++ 194-spring2013/hw1/src/Hw1.hs
@@ -1,19 +1,4 @@
-module Main where
-
-main :: IO ()
-main = do
-    putStrLn "homework 1..."
-    putStrLn $ "last digit 1234 = " ++ show (lastDigit 1234)
-    putStrLn $ "drop last digit 1234 = " ++ show (dropLastDigit 1234)
-    putStrLn $ "to digits 1234 = " ++ show (toDigits 1234)
-    putStrLn $ "doubleEveryOther [8,7,6,5] = " ++ show (doubleEveryOther [8,7,6,5])
-    putStrLn $ "sum of digits [16,7,12,5] = " ++ show (sumDigits $ doubleEveryOther [8,7,6,5])
-    putStrLn $ "validate 4012888888881881 True = " ++ show (validate 4012888888881881)
-    putStrLn $ "validate 4012888888881882 False = " ++ show (validate 4012888888881882)
-    putStrLn $ "hanoi 3 = " ++ show (hanoi 3 "from" "to" "temp")
-    putStrLn $ "hanoi4 4 = " ++ show (hanoi4 4 "from" "to" "temp1" "temp2")
-    putStrLn $ "32,767 moves for hanoi 3 peg 15 = " ++ show (length (hanoi 15 "from" "to" "temp") == 32767)
-    putStrLn $ "509 moves for hanoi 4 peg 15 = " ++ show (length (hanoi4 15 "from" "to" "temp1" "temp2") == 509)
+module Hw1 where
 
 -- exercise 1 ---------------
 lastDigit :: Integer -> Integer
@@ -63,5 +48,3 @@ hanoi4 n a b c d = hanoi4 (n-2) a c b d ++
                    hanoi4 (n-2) c b d a
 
 
-
---
--- 194-spring2013/hw2/app/Main.hs
+++ 194-spring2013/hw2/app/Main.hs
@@ -1,15 +1,17 @@
 module Main where
 
-import           Log         (testWhatWentWrong, testParse)
+import           Log         (testParse, testWhatWentWrong)
 import           LogAnalysis (parse, testSolution, whatWentWrong)
 
 main :: IO ()
 main = do
-    putStrLn "homework 2..."
-    testParsing <- testParse parse 100 "error.log"
+    putStrLn "CIS 194, Spring 2013 assignment 2"
+    putStrLn ""
+    putStrLn "test parse smaple.log"
+    testParsing <- testParse parse 100 "./data/sample.log"
     mapM_ print testParsing
-    error1  <- testWhatWentWrong parse whatWentWrong "error.log"
-    error2 <- testSolution "error.log"
+    error1  <- testWhatWentWrong parse whatWentWrong "./data/error.log"
+    error2 <- testSolution "./data/error.log"
     putStrLn "\ntake 3 from testWhatWentWrong on error.log:"
     putStrLn "["
     mapM_ print (take 3 error1)
@@ -20,3 +22,4 @@ main = do
     putStrLn $ "total results of testWhatWentWrong == testSolution: "
                ++ show (error1 == error2)
     putStrLn "\ndone..."
+
